19c6a87d1dba620729aaf5076fadd72e15ddddc8
venkatakartrhik <99868069+venkatakartrhik@users.noreply.github.com>
2023-06-09 15:23:04 +0530

    n=int(input()) s=str(n) print(s[::-1])

--- reverse_of_a_numbers.py
+++ reverse_of_a_numbers.py
@@ -1,14 +1,3 @@
-def reverse(num):
-    rev=0
-    while num:
-        d=num%10
-        num=num//10
-        rev=rev*10+d
-    return rev
-    if num==rev:
-        print("true")
-    else:
-        print("false")
-num=int(input())
-res=reverse(num)
-print(res)
+n=int(input())
+s=str(n)
+print(s[::-1])

dd7033609640c2329dfe75acc54fb145b1531964
venkatakartrhik <99868069+venkatakartrhik@users.noreply.github.com>
2023-06-09 14:12:54 +0530

    from math import lcm n,m=map(int,input().split()) print(lcm(n,m))

--- LCM.py
+++ LCM.py
@@ -1,5 +1,3 @@
-m,n=map(int,input().split())
-for i in range(1,m*n+1):
-    if(i%n==0 and i%m==0):
-        print(i)
-        break
+from math import lcm
+n,m=map(int,input().split())
+print(lcm(n,m))

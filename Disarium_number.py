868c7c8409438e595448d402603a5a63a0275ddf
venkatakartrhik <99868069+venkatakartrhik@users.noreply.github.com>
2023-06-11 15:06:29 +0530

    a=int(input()) n=a s=len(str(a)) c=0 for i in range(1,s+1):     d=n%10     c+=d**s     n=n//10     s-=1 if c==a:     print("True") else:     print("False")

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,17 +1,13 @@
 a=int(input())
-u=t=a
-i=0
-s=0
-while a!=0:
-    j=a%10
-    a=a//10
-    i=i+1
-j=0
-for i in range(i,0,-1):
-    j=u%10
-    s+=pow(j,i)
-    u=u//10
-if s==t:
+n=a
+s=len(str(a))
+c=0
+for i in range(1,s+1):
+    d=n%10
+    c+=d**s
+    n=n//10
+    s-=1
+if c==a:
     print("True")
 else:
     print("False")

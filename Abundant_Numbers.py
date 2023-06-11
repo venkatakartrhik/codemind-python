f3460dad9e38c88f10330caff6bc7823b93f1759
venkatakartrhik <99868069+venkatakartrhik@users.noreply.github.com>
2023-06-11 12:30:39 +0530

    n=int(input()) sum=0 for i in range(1,n):     if n%i==0:         sum=sum+i if sum>n:     print(True) else:     print(False)

--- Abundant_Numbers.py
+++ Abundant_Numbers.py
@@ -1,10 +1,9 @@
 n=int(input())
 sum=0
-for i in range(1,n-1):
+for i in range(1,n):
     if n%i==0:
         sum=sum+i
 if sum>n:
-    print("True")
+    print(True)
 else:
-    print("False")
-        
+    print(False)
